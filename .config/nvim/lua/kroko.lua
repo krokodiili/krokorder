f5cf6968fa7c30d00964e3cef568cb02d3c500ed
LM <laurimeltti@gmail.com>
2022-11-26 16:52:21 +0200

    init lua setup-

--- .config/nvim/lua/kroko.lua
+++ .config/nvim/lua/kroko.lua
@@ -3,6 +3,7 @@ local buf, win
 
 local position = 0
 
+
 local function center(str)
   local width = api.nvim_win_get_width(0)
   local shift = math.floor(width / 2) - math.floor(string.len(str) / 2)
